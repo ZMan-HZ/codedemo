3472a812819c347a1ab7ad59df4c05c099758c36
Alex Man <20871456+ZMan-HZ@users.noreply.github.com>
2020-08-14 15:16:47 +0800

    Update Demo2Tests.java

--- src/test/java/com/zzm/demos/demo/Demo2Tests.java
+++ src/test/java/com/zzm/demos/demo/Demo2Tests.java
@@ -32,9 +32,8 @@ class Demo2Tests {
     ProviderConsumer<List<ItemDO>> providerConsumer;
 
     /**
-     * 试题1：
-     * 注意： 假设sku数据很多, 无法将sku列表完全加载到内存中
-     * 在com.alibaba.mos.service.SkuReadServiceImpl中实现com.alibaba.mos.api.SkuReadService#loadSkus(com.alibaba.mos.api.SkuReadService.SkuHandler)
+     * 1：
+     * 在com.zzm.demos.service.SkuReadServiceImpl中实现com.zzm.demos.api.SkuReadService#loadSkus(com.zzm.demos.api.SkuReadService.SkuHandler)
      * 从/resources/data/data.xls读取数据并逐条打印数据
      */
     @Test
@@ -49,8 +48,7 @@ class Demo2Tests {
     }
 
     /**
-     * 试题2：
-     * 注意： 假设sku数据很多, 无法将sku列表完全加载到内存中
+     * 2：
      * 计算以下统计值:
      * 1、获取价格在最中间的任意一个skuId，假设所有sku的价格都是精确到1元且一定小于1万元
      * 2、每个渠道库存量为前五的skuId列表 例如( miao:[1,2,3,4,5],tmall:[3,4,5,6,7],intime:[7,8,4,3,1]
@@ -173,9 +171,8 @@ class Demo2Tests {
     }
 
     /**
-     * 试题3:
-     * 注意： 假设sku数据很多, 无法将sku列表完全加载到内存中
-     * 基于试题1, 在com.alibaba.mos.service.ItemAggregationProviderConsumer中实现一个生产者消费者, 将sku列表聚合为商品, 并通过回调函数返回,
+     * 3:
+     * 基于1, 在com.zzm.demos.service.ItemAggregationProviderConsumer中实现一个生产者消费者, 将sku列表聚合为商品, 并通过回调函数返回,
      * 聚合规则为：
      * 对于sku type为原始商品(ORIGIN)的, 按货号(artNo)聚合成ITEM
      * 对于sku type为数字化商品(DIGITAL)的, 按spuId聚合成ITEM
